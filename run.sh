8a78f8a9ea6533c144ed3504d5c81722858f44dd
samane <140152860+samane11e@users.noreply.github.com>
2026-08-03 09:14:59 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -18,7 +18,7 @@ echo "
 ###############################################################################
 "
 leo run new || exit
-----------------------tttttttttttttttttttttttWWWWaaaaaaaaaaaaaaaaaaaaaaa
+----------------------tttttttttttttttttttttttWWWWaaaaaaaaaa
 # Have the Player 1 make a move.
 echo "
 ###############################################################################
